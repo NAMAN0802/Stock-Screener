b0d149ccf93c3cf264a88107a917dd02b1c8c132
NAMAN0802 <gnaman694@gmail.com>
2025-08-03 16:51:28 +0530

    Docker compose file

--- data pipeline/models/__init__.py
+++ data pipeline/models/__init__.py
@@ -1,9 +1,9 @@
-from stocks import Stocks
-from ratios import Ratios
-from profitloss import ProfitLoss
-from shareholding import Shareholding
-from cashflow import CashFlow
-from balancesheet import BalanceSheet
-from quaterly_results import QuaterlyResults
+from .stocks import Stocks
+from .ratios import Ratios
+from .profitloss import ProfitLoss
+from .shareholding import Shareholding
+from .cashflow import CashFlow
+from .balancesheet import BalanceSheet
+from .quaterly_results import QuaterlyResults
 
 __all__=["Stocks","Ratios","ProfitLoss","Shareholding","CashFlow","BalanceSheet","QuaterlyResults"]
